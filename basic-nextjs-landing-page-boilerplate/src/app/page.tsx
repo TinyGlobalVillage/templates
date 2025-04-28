762294847b9be55d9c73296f40aae2fa1c074575
tinygvillage <connect@refusionist.com>
2025-04-28 09:56:27 -0700

    removed extra Layout tag in pagetsx

--- basic-nextjs-landing-page-boilerplate/src/app/page.tsx
+++ basic-nextjs-landing-page-boilerplate/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Layout from '@/app/layout';
-
 export default function Home() {
   return (
-    <Layout>
-      <h1>Welcome to Your Next.js + TypeScript + Styled-Components Project!</h1>
+    <>
+      <h1>
+        Welcome to Your Next.js + TypeScript +
+        Styled-Components Project!
+      </h1>
       <p>Start building something amazing today.</p>
-    </Layout>
+    </>
   );
 }
